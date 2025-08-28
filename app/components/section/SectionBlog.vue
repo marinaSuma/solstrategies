267258332d6092cbed58abacc905f4f1baf8dd12
Marina Quaranta <marina@MacBookAir.cust.communityfibre.co.uk>
2025-08-28 10:04:25 +0100

    Actualizo blog con posts animados

--- app/components/section/SectionBlog.vue
+++ app/components/section/SectionBlog.vue
@@ -4,39 +4,43 @@
     <div id="hero">
         <div class="container">
             <div class="w-l center text-center">
-                <h1 class="fadeinUp mb-5">Our Blog</h1>
-                <div class="w-s center text fadeinUp">
+                <h1 class="fmb-5" data-aos="fade-up">Our Blog</h1>
+                <div class="w-s center text" data-aos="fade-up">
                     <p>Stay in the loop with the latest in crypto, plus exclusive updates <br> on what we’re building.</p>
                 </div>
             </div>
         </div>
     </div>     
-      <section class="blog">
+    <section class="blog">
         <div class="container">
             <div class="w-l center">
-                <div class="row">
-                  <div
-                    v-for="(post, index) in posts"
-                    :key="index"
-                    class="col-sm-4 fadeinUp"
-                  >
-                    <div class="grid-box">
-                      <div class="img">
-                        <img :src="post.img" :alt="post.title" />
-                      </div>
-                      <div class="content">
-                        <div class="meta">{{ post.date }}</div>
-                        <h6>{{ post.title }}</h6>
-                        <div class="border-gradient">
-                          <a :href="post.url" class="btn">View Article</a>
-                        </div>
+              <div class="row">
+                <div
+                  v-for="(post, index) in posts"
+                  :key="post.id"
+                  class="col-sm-4 post aos-init"
+                  data-aos="fade-up"
+                  :data-aos-delay="index * 150"
+                  data-aos-duration="800"
+                >
+                  <div class="grid-box">
+                    <div class="img">
+                      <img :src="post.img" :alt="post.title" />
+                    </div>
+                    <div class="content">
+                      <div class="meta">{{ post.date }}</div>
+                      <h6>{{ post.title }}</h6>
+                      <div class="border-gradient">
+                        <a :href="post.url" class="btn">View Article</a>
                       </div>
                     </div>
                   </div>
                 </div>
+              </div>
             </div>
         </div>
-      </section>
+    </section>
+
 
 </template>
 
