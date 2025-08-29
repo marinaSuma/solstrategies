4cf9aeb30c73942d004995c52490de05c0c2bcd4
Marina Quaranta <marina@MacBook-Air-de-Marina-2.local>
2025-08-29 13:17:00 +0200

    SCSS v3

--- app/components/section/SectionOverviewInvestors.vue
+++ app/components/section/SectionOverviewInvestors.vue
@@ -43,6 +43,9 @@
 </template>
 
 <script setup>
+let ctx;
+
+const el = useTemplateRef('el');
 
 const tradingViewOptions = {
   theme: 'light',
@@ -51,78 +54,7 @@ const tradingViewOptions = {
 };
 
 const symbolInfo = 'OTC:CYFRD';
-const symbolsTape = [
-  { proName: symbolInfo },
-  { proName: 'CRYPTO:BTCUSD' },
-  { proName: 'CRYPTO:SOLUSD' },
-];
+const symbolsTape = [{ proName: symbolInfo }, { proName: 'CRYPTO:BTCUSD' }, { proName: 'CRYPTO:SOLUSD' }];
+
 </script>
 
-<style scoped lang="scss">
-.overview {
-  position: relative;
-  padding: fn.toVw(122) 0;
-  text-align: center;
-
-  @include mx.mobile {
-    padding: fn.toVw(118) 0;
-  }
-}
-
-.bg {
-  pointer-events: none;
-
-  position: absolute;
-  z-index: -1;
-  bottom: fn.toVw(-500);
-  left: 0;
-
-  width: fn.toVw(816);
-
-  @include mx.mobile {
-    display: none;
-  }
-}
-
-.title {
-  margin-bottom: fn.toVw(-150);
-
-  @include mx.mobile {
-    margin-bottom: fn.toVw(-100);
-  }
-}
-
-.ticker {
-  display: flex;
-  flex-direction: column;
-  gap: fn.toVw(64);
-  justify-content: center;
-
-  margin-top: fn.toVw(-200);
-
-  @include mx.mobile {
-    gap: 0;
-    margin-top: fn.toVw(-100);
-  }
-
-  .ticker__symbol {
-    width: 100%;
-    max-width: 600px;
-    height: 230px;
-    margin: 0 auto;
-
-    @include mx.mobile {
-      display: none;
-    }
-  }
-
-  .ticker__tape {
-    height: 46px;
-    margin-top: calc(fn.toVw(98) - fn.toVw(64));
-
-    @include mx.mobile {
-      margin-top: fn.toVw(46);
-    }
-  }
-}
-</style>
