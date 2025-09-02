17712379948a40bcbdfb1c52a8b1b55ca768b741
Marina Quaranta <marina@MacBook-Air-de-Marina-2.local>
2025-09-03 00:38:05 +0200

    Events v6

--- app/components/section/SectionOverviewInvestors.vue
+++ app/components/section/SectionOverviewInvestors.vue
@@ -58,3 +58,80 @@ const symbolsTape = [{ proName: symbolInfo }, { proName: 'CRYPTO:BTCUSD' }, { pr
 
 </script>
 
+<style scoped lang="scss">
+  .overview {
+    position: relative;
+    padding: fn.toVw(122) 0;
+    text-align: center;
+
+    @include mx.mobile {
+      padding: fn.toVw(118) 0;
+    }
+
+
+    .bg {
+      pointer-events: none;
+
+      position: absolute;
+      z-index: -1;
+      bottom: fn.toVw(-500);
+      left: 0;
+
+      width: fn.toVw(816);
+
+      @include mx.mobile {
+        display: none;
+      }
+
+  
+
+    }
+
+
+  
+
+    .title {
+      // margin-bottom: fn.toVw(-150);
+
+      // @include mx.mobile {
+      //   margin-bottom: fn.toVw(-100);
+      // }
+    }
+
+    .ticker {
+      display: flex;
+      flex-direction: column;
+      gap: fn.toVw(64);
+      justify-content: center;
+
+      margin-top:70px;
+
+      @include mx.mobile {
+        gap: 0;
+        margin-top: fn.toVw(-100);
+      }
+
+      .ticker__symbol {
+        width: 100%;
+        max-width: 600px;
+        height: 230px;
+        margin: 0 auto;
+
+        @include mx.mobile {
+          display: none;
+        }
+      }
+
+      .ticker__tape {
+        height: 46px;
+        // margin-top: calc(fn.toVw(98) - fn.toVw(64));
+        margin-top: 60px;
+
+        @include mx.mobile {
+          // margin-top: fn.toVw(46);
+          margin-top: 40px;
+        }
+      }
+    }
+  }
+</style>
