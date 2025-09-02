9d5c5d02e965bb8251ebb96a4b4344cf5aed2aaf
Marina Quaranta <marina@MacBook-Air-de-Marina-2.local>
2025-09-02 21:47:44 +0200

    cards y bg

--- app/components/card/CardWhyInvestors.vue
+++ app/components/card/CardWhyInvestors.vue
@@ -14,3 +14,18 @@ const props = defineProps({
   background: String
 });
 </script>
+
+<style scoped lang="scss">
+
+
+.bg {
+  position: absolute;
+  z-index: 0;
+  inset: 0;
+
+  width: 100%;
+  height: 100%;
+}
+
+
+</style>
