c6be49f35cfa9e9aed517981062259755a6e8c10
Marina Quaranta <marina@MacBookAir.fritz.box>
2025-09-09 00:01:12 +0200

    About?

---
+++ app/pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="$route.name">
+    <SectionHeroAbout />
+    <SectionCta />
+  </div>
+</template>
+
+<script setup>
+import seo from '~~/config/seo.json';
+import '~/assets/styles/aboutStyles.scss' 
+
+useHead({
+  titleTemplate: null,
+});
+
+const title = seo.title;
+
+useSeoMeta({
+  title: title,
+  ogTitle: title,
+});
+</script>
