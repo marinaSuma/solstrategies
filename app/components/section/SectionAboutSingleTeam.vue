67d2c546882283b4371fc7ec2419c92b3dbb005f
Marina Quaranta <marina@MacBook-Air-de-Marina-2.local>
2025-09-10 16:25:56 +0200

    upload Team inicio

---
+++ app/components/section/SectionAboutSingleTeam.vue
@@ -0,0 +1,36 @@
+<template>
+  <section ref="el" class="team">
+    <div class="container">
+      <div class="singleTeam">
+        <div class="img">
+          <MediaImg src="/bio.svg" alt="Leah Wald" />
+        </div>
+        
+        <div class="content">
+          <Text tag="h3" data-split data-linereveal>Leah Wald</Text>
+          <Text tag="h6" class="titleDown" data-split data-linereveal>President / CEO</Text>
+          <Text tag="p" class="text"  data-split data-linereveal reveal-notrigger reveal-waitpreloader reveal-delay="0.18"></Text>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<script setup>
+
+
+const el = useTemplateRef('el');
+
+let ctx;
+
+
+</script>
+
+<style scoped lang="scss">
+section.team {
+   @include mx.mobile {
+  }
+}
+
+</style>
