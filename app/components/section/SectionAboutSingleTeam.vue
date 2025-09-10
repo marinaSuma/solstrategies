5c6f97cf13e3f67e8b38ff91d1173642c131d44c
Marina Quaranta <marina@MacBook-Air-de-Marina-2.local>
2025-09-10 20:30:00 +0200

    upload Team team 1

--- app/components/section/SectionAboutSingleTeam.vue
+++ app/components/section/SectionAboutSingleTeam.vue
@@ -1,5 +1,9 @@
 <template>
   <section ref="el" class="team">
+    <div class="bg-bottom">
+      <MediaImg src="/teamSingleBG.png" alt="Team Background" />
+    </div>
+
     <div class="container">
       <div class="singleTeam">
         <div class="img">
@@ -9,7 +13,9 @@
         <div class="content">
           <Text tag="h3" data-split data-linereveal>Leah Wald</Text>
           <Text tag="h6" class="titleDown" data-split data-linereveal>President / CEO</Text>
-          <Text tag="p" class="text"  data-split data-linereveal reveal-notrigger reveal-waitpreloader reveal-delay="0.18"></Text>
+          <Text tag="p" class="text"  data-split data-linereveal reveal-notrigger reveal-waitpreloader reveal-delay="0.18">
+            Leah Wald is an accomplished entrepreneur with deep expertise in alternative asset management and disruptive technologies. As Co-Founder and CEO of Valkyrie Investments, she led the firm to over $1.3 billion in AUM through cryptocurrency trusts, hedge funds, and ETFs. Valkyrie’s advisory business was acquired by CoinShares, and its private trust division by Abra Capital in 2024. Previously, Leah spearheaded multi-billion-dollar investment strategies at Vital Financial and served as a Partner at Lucid Investment Strategies. Ms. Wald is a contributing writer for Forbes and co-authored the book Hyperwave Theory: The Rogue Waves of Financial Markets. She holds an MS from IE Business School and a BA from Pitzer College.
+          </Text>
         </div>
       </div>
     </div>
@@ -32,5 +38,15 @@ section.team {
    @include mx.mobile {
   }
 }
+
+
+.bg {
+  top: 0;
+  left: 0;
+  position: absolute;
+  z-index: 0;
+  opacity: 0.8;
+}
+
 
 </style>
